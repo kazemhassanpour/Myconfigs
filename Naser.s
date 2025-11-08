21584f1b72a8f656d19441a52608d27ea4e46068
kazemhassanpour <hassanpour_kazem@yahoo.com>
2025-11-08 10:56:16 +0330

    Update Naser.s

--- Naser.s
+++ Naser.s
@@ -1,1 +1,2 @@
 vless://2b3db85d-85f2-4180-c215-15d51ec89593@89.34.230.136:18853?type=tcp&security=none#K%20VPN-Naser.s
+vless://2b3db85d-85f2-4180-c215-15d51ec89593@[2a0e:97c0:180:5b::1]:18853?type=tcp&security=none#Naser.s
