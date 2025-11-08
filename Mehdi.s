f3ff864fb2d9c6e304b934a9e92b7587f82d64b1
kazemhassanpour <hassanpour_kazem@yahoo.com>
2025-11-08 10:55:10 +0330

    Update Mehdi.s

--- Mehdi.s
+++ Mehdi.s
@@ -1,1 +1,2 @@
 vless://43c862ef-5a8b-4e5d-b372-dfb4842beca7@89.34.230.136:18853?type=tcp&security=none#K%20VPN-Mehdi.s
+vless://43c862ef-5a8b-4e5d-b372-dfb4842beca7@[2a0e:97c0:180:5b::1]:18853?type=tcp&security=none#Mehdi.s
