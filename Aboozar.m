7c7c407ae7675b584e4de4544db398884efd16e2
kazemhassanpour <hassanpour_kazem@yahoo.com>
2025-11-08 09:21:04 +0330

    Update Aboozar.m

--- Aboozar.m
+++ Aboozar.m
@@ -1,2 +1,2 @@
 vless://18fd4d69-d013-44c1-e74a-655d21d6794f@89.34.230.136:18853?type=tcp&security=none#K%20VPN-Aboozar.m
-vless://7cf92f0f-3145-4cb0-9ac4-47c2f937614d@er1.v-sub.site:1517?security=none&encryption=none&host=varzesh3.com&headerType=http&type=tcp#Aboozar.m
+vless://18fd4d69-d013-44c1-e74a-655d21d6794f@[2a0e:97c0:180:5b::1]:18853?type=tcp&security=none#Aboozar.m
