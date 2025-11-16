8f5122db58810da8a6c286363be3b63bbb7eddd7
kazemhassanpour <hassanpour_kazem@yahoo.com>
2025-11-16 11:00:34 +0330

    Update Ahmad.f

--- Ahmad.f
+++ Ahmad.f
@@ -1,2 +1,3 @@
 vless://14d1b333-92bf-4e16-b40f-a841818f8b6e@89.34.230.136:18853?type=tcp&security=none#K%20VPN-Ahmad.f
 vless://14d1b333-92bf-4e16-b40f-a841818f8b6e@[2a0e:97c0:180:5b::1]:18853?type=tcp&security=none#Ahmad.f
+vless://79862a9d-36fa-48ee-9af6-efd015508b99@er1.v-sub.site:1517?security=none&encryption=none&host=varzesh3.com&headerType=http&type=tcp#Ahmad.f+1
