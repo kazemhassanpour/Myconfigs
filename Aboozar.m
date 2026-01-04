ee6be958af672a1a739ec956f515303965fe5152
kazemhassanpour <hassanpour_kazem@yahoo.com>
2026-01-04 09:18:37 +0330

    Update Aboozar.m

--- Aboozar.m
+++ Aboozar.m
@@ -1,2 +1,3 @@
 vless://18fd4d69-d013-44c1-e74a-655d21d6794f@89.34.230.136:18853?type=tcp&security=none#K%20VPN-Aboozar.m
 vless://18fd4d69-d013-44c1-e74a-655d21d6794f@[2a0e:97c0:180:5b::1]:18853?type=tcp&security=none#Aboozar.m
+vless://2961dd8b-dbae-447c-b38c-4a8011698d2a@er1.v-sub.site:1517?security=none&encryption=none&host=speedtest.net&headerType=http&type=tcp#Aboozar.m+1
