6e41b4241c5df75a64914c79f2a793c3982a26a9
kazemhassanpour <hassanpour_kazem@yahoo.com>
2026-01-25 10:08:22 +0330

    Update Mehdi.s

--- Mehdi.s
+++ Mehdi.s
@@ -1,2 +1,3 @@
 vless://43c862ef-5a8b-4e5d-b372-dfb4842beca7@89.34.230.136:18853?type=tcp&security=none#K%20VPN-Mehdi.s
 vless://43c862ef-5a8b-4e5d-b372-dfb4842beca7@[2a0e:97c0:180:5b::1]:18853?type=tcp&security=none#Mehdi.s
+vless://fab8461d-b2d9-445f-9482-b91423386b42@ert-9.v-sub.site:8880?path=%2F%3Fed%3D2080&security=none&encryption=none&host=admin.whaletm2.site&type=httpupgrade#Mehdi.s+1
