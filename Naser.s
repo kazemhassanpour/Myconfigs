fa8cb97767de4fc7b6b582507abd50f49ec508ed
kazemhassanpour <hassanpour_kazem@yahoo.com>
2026-01-25 11:24:43 +0330

    Update Naser.s

--- Naser.s
+++ Naser.s
@@ -1,2 +1,3 @@
 vless://2b3db85d-85f2-4180-c215-15d51ec89593@89.34.230.136:18853?type=tcp&security=none#K%20VPN-Naser.s
 vless://2b3db85d-85f2-4180-c215-15d51ec89593@[2a0e:97c0:180:5b::1]:18853?type=tcp&security=none#Naser.s
+vless://1f04d74f-8f6a-449d-bc8b-4923ec1f26c2@ert-9.v-sub.site:8880?path=%2F%3Fed%3D2080&security=none&encryption=none&host=admin.whaletm2.site&type=httpupgrade#Naser.s+1
